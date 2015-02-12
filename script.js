87af160948b7f006deaf93acd4ea8fc2e62293e2
addiefuller <AdelaideFuller1@Adelaide-Fullers-MacBook-Pro-7.local>
2015-02-11 19:00:47 -0500

    Geojson

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var addiesfavoritethings = ["colors", "architecture", "snow", "ice cream"]
+// for loop through an array
+for (var i = 0; i < addiesfavoritethings.length; i++) {
+	console.log("Addie Likes", addiesfavoritethings[i]);
+}
+
+
